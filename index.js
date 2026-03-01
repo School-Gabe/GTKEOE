5d9a58d5bf5333fe7621106200140af4ac567310
School-Gabe <gabrielmspears@lewisu.edu>
2026-03-01 14:01:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Starting Hello, World.")
 
-function makeGreen() {
-	console.log("Calling makeGreen function")
-	document.getElementById("msg").classList.add("green");
+function makeBlue() {
+	console.log("Calling makeBlue function")
+	document.getElementById("msg").classList.add("blue");
 }
